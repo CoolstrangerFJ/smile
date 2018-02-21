cdb4062da06a3852722275fd9a44a30d594b513e
fengjian <fengjian3434@foxmail.com>
2018-02-21 16:37:32 +0800

    并把选择器的相同代码抽取出来，放到了AbstractSelector中

--- src/main/java/connecter/selector/InputSelector.java
+++ src/main/java/connecter/selector/InputSelector.java
@@ -6,13 +6,8 @@ package connecter.selector;
 import java.io.IOException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
-import java.util.Iterator;
-import java.util.Set;
 
 import connecter.processor.IProcessor;
-import factory.ProcessorFactory;
 
 /**
  * @author CoolStranger
@@ -20,54 +15,18 @@ import factory.ProcessorFactory;
  * @time 下午4:43:38
  *
  */
-public class InputSelector implements Runnable {
+public class InputSelector extends AbstractSelector {
 
-	private Selector selector;
-	private OutputSelector outputSelector;
-
-	public InputSelector(Selector selector, OutputSelector outputSelector) {
-		this.selector = selector;
-		this.outputSelector = outputSelector;
+	public InputSelector(Selector selector) {
+		super(selector, SelectionKey.OP_READ);
 	}
 
-	private void work() {
-		while (true) {
-			try {
-				selector.select();
-				//从选择器中获取事件
-				Set<SelectionKey> selectedKeys = selector.selectedKeys();
-				Iterator<SelectionKey> iterator = selectedKeys.iterator();
-				//遍历事件集合
-				while (iterator.hasNext()) {
-					SelectionKey key = iterator.next();
-					iterator.remove();
-
-					//根据事件类型处理
-					if (key.isAcceptable()) {
-						ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
-						SocketChannel socketChannel = serverSocketChannel.accept();
-						socketChannel.configureBlocking(false);
-						SelectionKey newKey = socketChannel.register(selector, SelectionKey.OP_READ);
-						IProcessor processor = ProcessorFactory.createProcessor(socketChannel, outputSelector);
-						newKey.attach(processor);
-					} else if (key.isReadable()) {
-						IProcessor processor = (IProcessor) key.attachment();
-						processor.tryRead();
-					}
-				}
-			} catch (IOException e) {
-			} catch (Throwable e) {
-				// e.printStackTrace();
-			}
-		}
-	}
-
-	/**
-	 * @see java.lang.Runnable#run()
-	 */
 	@Override
-	public void run() {
-		work();
+	protected void handle(SelectionKey key) throws IOException {
+		if (key.isReadable()) {
+			IProcessor processor = (IProcessor) key.attachment();
+			processor.tryRead();
+		}
 	}
 
 }
--- src/main/java/connecter/selector/OutputSelector.java
+++ src/main/java/connecter/selector/OutputSelector.java
@@ -3,15 +3,8 @@
  */
 package connecter.selector;
 
-import java.io.IOException;
-import java.nio.channels.CancelledKeyException;
-import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.concurrent.ConcurrentLinkedQueue;
-//import java.util.concurrent.atomic.AtomicBoolean;
 
 import connecter.processor.IProcessor;
 
@@ -21,78 +14,21 @@ import connecter.processor.IProcessor;
  * @time 下午4:43:38
  *
  */
-public class OutputSelector implements Runnable {
+public class OutputSelector extends AbstractSelector {
 
-	private ConcurrentLinkedQueue<IProcessor> processorQueue = new ConcurrentLinkedQueue<>();
-	private Selector writeSelector;
-
-	public OutputSelector(Selector writeSelector) {
-		this.writeSelector = writeSelector;
+	public OutputSelector(Selector selector) {
+		super(selector, SelectionKey.OP_WRITE);
 	}
 
-	private void work() {
-		int count = 0;
-		IProcessor wait4write = null;
-		while (true) {
-			try {
-				count = writeSelector.selectNow();
-				// 注册队列中的通道
-				while ((wait4write = processorQueue.poll()) != null) {
-					try {
-						wait4write.getSocketChannel().register(writeSelector, SelectionKey.OP_WRITE, wait4write);
-						count++;
-					} catch (ClosedChannelException | CancelledKeyException e) {
-					}
-				}
-				count += writeSelector.selectNow();
-
-				if (count == 0) {
-					// 若无事件,休息一下能极大地降低CPU性能浪费
-					Thread.sleep(1);
-					continue;
-				}
-
-				Set<SelectionKey> selectedKeys = writeSelector.selectedKeys();
-				Iterator<SelectionKey> iterator = selectedKeys.iterator();
-				while (iterator.hasNext()) {
-					SelectionKey key = iterator.next();
-					iterator.remove();
-
-					if (key.isWritable()) {
-						IProcessor processor = (IProcessor) key.attachment();
-						processor.tryWrite();
-					}
-				}
-			} catch (IOException e) {
-			} catch (Throwable e) {
-				// e.printStackTrace();
-			}
+	@Override
+	protected void handle(SelectionKey key) {
+		if (key.isWritable()) {
+			IProcessor processor = (IProcessor) key.attachment();
+			processor.tryWrite();
 		}
 	}
 
-	/**
-	 * @see java.lang.Runnable#run()
-	 */
-	@Override
-	public void run() {
-		work();
-	}
-
-	public void addWriteTask(IProcessor processor) {
-		processorQueue.add(processor);
-	}
-
 	public Selector getSelector() {
-		return writeSelector;
+		return selector;
 	}
-
-	// public void selectNow() {
-	// try {
-	// if (writeSelector.selectNow() != 0) {
-	// hasKey.set(true);
-	// }
-	// } catch (IOException e) {
-	// throw new RuntimeException(e);
-	// }
-	// }
 }
